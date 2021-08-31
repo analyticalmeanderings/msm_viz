759b83bd809b9cfbdf7c4e447c7d5b8d5224ad55
Matthew Christian <matthew.christian@USP.org>
2021-08-31 11:18:46 -0400

    adding todo for bot

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -3,3 +3,10 @@ import os
 
 # TODO build a WhatsApp bot
 # TODO rip off code from https://www.youtube.com/watch?v=_z86t7LerrQ&t=808s
+# TODO Kernal text from https://www.usp.org/supply-chain/medicine-supply-map, what does the next page look like?
+# TODO "The article I didn't write" by Carlos Fernandez
+'''
+I did not write the attached article, my code did.
+
+Hi, I'm Carlos ... 
+'''
